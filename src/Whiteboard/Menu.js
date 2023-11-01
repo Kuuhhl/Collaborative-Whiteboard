7d3900c411b8338e957fa86d7171dc9d0330a7e3
philipp <50250716+Kuuhhl@users.noreply.github.com>
2023-11-01 17:23:19 +0000

    set up tailwindcss

--- src/Whiteboard/Menu.js
+++ src/Whiteboard/Menu.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Menu = () => {
-    return <div className='menu_container'></div>
+	return (
+		<div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-96 bg-gray-200 rounded-b-lg h-16 flex justify-around items-center"></div>
+	);
 };
 
 export default Menu;
